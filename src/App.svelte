d9442614c75e99af375b4a80598a48cfabbefb8a
youyoung00 <you889@hanmail.net>
2022-09-09 03:27:05 +0900

    라이프 사이클 테스트 코드 작성

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { lifecycle, delayRender } from './lifecycle.js'
+	import Something from './Something.svelte'
+
+	let done = delayRender()
+	lifecycle()
+</script>
+
+{#if $done}
+  <h1>Hello Lifecycle!</h1>
+{/if} 
+
+<Something />
